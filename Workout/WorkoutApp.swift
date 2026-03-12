0fac9b5d25fdd14ccfdc3908919dc43bfb2c0459
OrDinaD <Vlad.vasilevskiy.07@gmail.com>
2026-03-12 12:39:37 +0300

    Initial Commit

---
+++ Workout/WorkoutApp.swift
@@ -0,0 +1,17 @@
+//
+//  WorkoutApp.swift
+//  Workout
+//
+//  Created by Влад on 12.03.26.
+//
+
+import SwiftUI
+
+@main
+struct WorkoutApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
